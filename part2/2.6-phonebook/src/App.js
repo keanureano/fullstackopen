b59aa87cc9369dcfe4ca84795cbfd7f9c420c5e4
knreano <112625935+knreano@users.noreply.github.com>
2023-01-25 18:48:52 +0800

    part2

--- part2/2.6-phonebook/src/App.js
+++ part2/2.6-phonebook/src/App.js
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Form from "./components/Form";
 import Filter from "./components/Filter";
 import Person from "./components/Person";
 import Header from "./components/Header";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+  useEffect(() => {
+    console.log("use effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
   const filterPersons =
     filterName.trim() === ""
       ? persons
