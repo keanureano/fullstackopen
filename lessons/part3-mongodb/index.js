b6367f641191b8b3552be91a1601171aed9b618a
Keanu Reanom <reanokeanu@gmail.com>
2023-02-18 21:05:17 +0800

    fullstackopen

--- lessons/part3-mongodb/index.js
+++ lessons/part3-mongodb/index.js
@@ -1,35 +1,10 @@
-const mongoose = require("mongoose");
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
+const Note = require("./models/note");
 
 const app = express();
 app.use(express.json(), morgan("tiny"));
-
-const password = process.argv[2];
-const url = `mongodb+srv://reanokeanu:${password}@cluster0.wehmws4.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Note = mongoose.model("Note", noteSchema);
 
 app.get("/api/notes", (req, res) => {
   Note.find({}).then((notes) => {
@@ -37,7 +12,30 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-const PORT = 3001;
+app.get("/api/notes/:id", (req, res) => {
+  Note.findById(req.params.id).then((note) => {
+    res.json(note);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (body.content === undefined) {
+    return res.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
