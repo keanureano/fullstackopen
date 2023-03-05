545f115072edc1e26f7f154e4b2eb45fa967068f
Keanu Reanom <reanokeanu@gmail.com>
2023-03-05 10:48:42 +0800

    bloglist step2

--- exercises/part4/4.1-bloglist/app.js
+++ exercises/part4/4.1-bloglist/app.js
@@ -27,38 +27,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 module.exports = app;
-
-
-// require("dotenv").config();
-
-// const http = require("http");
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const morgan = require("morgan");
-
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number,
-// });
-
-// const Blog = mongoose.model("Blog", blogSchema);
-
-// mongoose.set("strictQuery", false);
-// console.log("connecting to", process.env.MONGODB_URI);
-// mongoose
-//   .connect(process.env.MONGODB_URI)
-//   .then(() => {
-//     console.log("connected to mongoDB");
-//   })
-//   .catch((error) => {
-//     console.error("error connecting to mongoDB:", error.message);
-//   });
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("tiny"));
--- exercises/part4/4.1-bloglist/utils/middleware.js
+++ exercises/part4/4.1-bloglist/utils/middleware.js
@@ -1,4 +1,15 @@
+const logger = require("./logger");
+
 const morgan = require("morgan");
 const requestLogger = morgan("tiny");
 
-module.exports = { requestLogger };
+const unknownEndpoint = (req, res) => {
+  return res.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+  next(error);
+};
+
+module.exports = { requestLogger, unknownEndpoint, errorHandler };
