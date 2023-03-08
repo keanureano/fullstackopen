4aabf79bbb7aac095e2769290376471782d7be6e
Keanu Reaño <reanokeanu@gmail.com>
2023-03-08 12:51:09 +0800

    lessons part4 b

---
+++ lessons/part4-supertest/app.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const express = require("express");
+require("express-async-errors");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+
+mongoose.set("strictQuery", false);
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to mongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to mongoDB:", error.message);
+  });
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
