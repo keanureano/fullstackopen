2745c4f297074e15f9ca4f04fe2b110756339c16
Keanu Reaño <reanokeanu@gmail.com>
2023-03-08 14:12:49 +0800

    bloglist tests step3

--- exercises/part4/4.1-bloglist/tests/blog_api.test.js
+++ exercises/part4/4.1-bloglist/tests/blog_api.test.js
@@ -24,6 +24,27 @@ describe("blog api", () => {
     expect(blogs[0].id).toBeDefined();
     expect(blogs[0]._id).toBe(undefined);
   });
+
+  test("valid blog can be added", async () => {
+    const newBlog = {
+      title: "New Title",
+      author: "New Author",
+      url: "https://newurl.com/",
+      likes: 0,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogs = await helper.getBlogs();
+    expect(blogs.length).toEqual(helper.initialBlogs.length + 1);
+    
+    const blogTitles = blogs.map((blog) => blog.title);
+    expect(blogTitles).toContain("New Title");
+  });
 });
 
 afterAll(async () => {
