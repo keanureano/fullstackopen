1878a056f0364250e4dbdbdc18bc5adec47bbabc
Keanu Reaño <reanokeanu@gmail.com>
2023-03-11 09:53:56 +0800

    bloglist expansions step2

--- exercises/part4/4.1-bloglist/utils/middleware.js
+++ exercises/part4/4.1-bloglist/utils/middleware.js
@@ -10,6 +10,9 @@ const unknownEndpoint = (req, res) => {
 const errorHandler = (error, req, res, next) => {
   if (error.name === "ValidationError") {
     return res.status(400).json({ error: error.message });
+  }
+  if (error.name === "CastError") {
+    return res.status(400).json({ error: error.message });
   } else {
     logger.error(error.message);
   }
