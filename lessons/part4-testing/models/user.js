6c6c55f7e9bb311ebe524efd95ac7f562d7ad15c
Keanu Reaño <reanokeanu@gmail.com>
2023-03-15 12:14:12 +0800

    fix eslint

---
+++ lessons/part4-testing/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id,
+    delete returnedObject.passwordHash,
+    delete returnedObject.__v;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- lessons/part4-testing/mongo.js
+++ lessons/part4-testing/mongo.js
@@ -1,36 +1,35 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/testing?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/testing?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: 'CSS is hard',
+  content: "CSS is hard",
   date: new Date(),
   important: true,
-})
+});
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+note.save().then((result) => {
+  console.log("note saved!", result);
+  mongoose.connection.close();
+});
 /*
 
 Note.find({}).then(result => {
