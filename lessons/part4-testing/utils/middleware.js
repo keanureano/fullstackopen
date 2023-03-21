b3202e92b918576445e7a4919dca9b2870855739
Keanu Reaño <reanokeanu@gmail.com>
2023-03-21 21:44:59 +0800

    lesson 4d limiting created notes to logged in users

--- lessons/part4-testing/utils/middleware.js
+++ lessons/part4-testing/utils/middleware.js
@@ -17,10 +17,14 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
+  }
+  if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
   }
-
+  if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: error.message });
+  }
+  
   next(error);
 };
 
