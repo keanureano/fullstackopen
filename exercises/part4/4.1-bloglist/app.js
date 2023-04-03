bd36513f540540d713c9bae4375e28a9f70d508a
Keanu Reaño <reanokeanu@gmail.com>
2023-04-03 16:51:23 +0800

    bloglist expansion, step8

--- exercises/part4/4.1-bloglist/app.js
+++ exercises/part4/4.1-bloglist/app.js
@@ -26,7 +26,9 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
--- exercises/part4/4.1-bloglist/utils/middleware.js
+++ exercises/part4/4.1-bloglist/utils/middleware.js
@@ -22,4 +22,17 @@ const errorHandler = (error, req, res, next) => {
   next(error);
 };
 
-module.exports = { requestLogger, unknownEndpoint, errorHandler };
+const tokenExtractor = (req, res, next) => {
+  const auth = req.get("Authorization");
+  if (auth && auth.startsWith("Bearer ")) {
+    req.token = auth.replace("Bearer ", "");
+  }
+  next();
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+};
