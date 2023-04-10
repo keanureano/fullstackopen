f607211db8b96f12fd6b7e6733b4d97a988a436a
Keanu Reaño <reanokeanu@gmail.com>
2023-04-10 19:27:21 +0800

    rename folders1

---
+++ exercises/part5/5.1-bloglist-frontend/src/App.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  return (
+    <div>
+      {user === null && <LoginForm setUser={setUser} />}
+      {user !== null && <Blogs user={user} />}
+    </div>
+  );
+};
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="text"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+const Blogs = ({ user }) => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  return (
+    <div>
+      <p>{user.username} logged in</p>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
