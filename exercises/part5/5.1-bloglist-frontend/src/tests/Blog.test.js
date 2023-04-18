c0d78845dec0c4bb48a4dd00c7e251c064b84246
Keanu Reaño <reanokeanu@gmail.com>
2023-04-18 22:59:47 +0800

    blog list tests, step3

--- exercises/part5/5.1-bloglist-frontend/src/tests/Blog.test.js
+++ exercises/part5/5.1-bloglist-frontend/src/tests/Blog.test.js
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom/extend-expect";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Blog from "../components/Blog";
 import blogService from "../services/blogs";
+import { act } from "@testing-library/react";
 
 describe("blog", () => {
   const blog = {
@@ -13,6 +14,11 @@ describe("blog", () => {
     user: { username: "Username" },
   };
   let container;
+
+  const toggleBlog = async () => {
+    const blogToggleButton = container.querySelector(".blog-toggle-btn");
+    await fireEvent.click(blogToggleButton);
+  };
 
   beforeEach(() => {
     container = render(<Blog blog={blog} />).container;
@@ -33,11 +39,26 @@ describe("blog", () => {
   });
 
   test("should render url and likes when show button is clicked", async () => {
-    const blogToggleButton = container.querySelector(".blog-toggle-btn");
-    await fireEvent.click(blogToggleButton);
+    await toggleBlog();
     const blogUrl = container.querySelector(".blog-url");
     const blogLikes = container.querySelector(".blog-likes");
     expect(blogUrl).toBeDefined();
     expect(blogLikes).toBeDefined();
+  });
+
+  test("should render url and likes when show button is clicked", async () => {
+    const blogServiceSpy = jest
+      .spyOn(blogService, "put")
+      .mockResolvedValueOnce({ ...blog, likes: blog.likes + 1 });
+
+    await toggleBlog();
+    const likeButton = container.querySelector(".blog-like-btn");
+
+    await act(() => {
+      fireEvent.click(likeButton);
+      fireEvent.click(likeButton);
+    });
+
+    expect(blogServiceSpy).toHaveBeenCalledTimes(2);
   });
 });
