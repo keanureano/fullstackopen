54188ea437fd78501dc08ee657221a7c6499cbb5
Keanu Reaño <reanokeanu@gmail.com>
2023-04-30 23:27:33 +0800

    better anecdotes, step8 step9

--- exercises/part6/6.3-anecdotes-redux/src/components/AnecdoteForm.js
+++ exercises/part6/6.3-anecdotes-redux/src/components/AnecdoteForm.js
@@ -1,22 +1,22 @@
 import { useDispatch } from "react-redux";
-import { newAnecdote } from "../reducers/anecdoteReducer";
+import { createAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const createAnecdote = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
 
     const content = event.target.content.value;
     event.target.content.value = "";
 
-    dispatch(newAnecdote(content));
+    dispatch(createAnecdote(content));
   };
 
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
+      <form onSubmit={submitHandler}>
         <div>
           <input name="content" />
         </div>
--- exercises/part6/6.3-anecdotes-redux/src/index.js
+++ exercises/part6/6.3-anecdotes-redux/src/index.js
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { legacy_createStore as createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
-import anecdoteReducer from "./reducers/anecdoteReducer";
-import filterReducer from "./reducers/filterReducer";
+import store from "./store";
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
-});
-
-const store = createStore(reducer);
 store.subscribe(() => console.dir(store.getState()));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
