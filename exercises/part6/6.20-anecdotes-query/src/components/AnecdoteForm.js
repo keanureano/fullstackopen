2b3d9107082a03f2e7b459e250af53b14b4983f5
Keanu Reaño <reanokeanu@gmail.com>
2023-05-04 21:22:06 +0800

    anecdotes query step5

--- exercises/part6/6.20-anecdotes-query/src/components/AnecdoteForm.js
+++ exercises/part6/6.20-anecdotes-query/src/components/AnecdoteForm.js
@@ -1,16 +1,21 @@
 import { useMutation, useQueryClient } from "react-query";
 import { createAnecdote } from "../services/anecdotes";
+import { useNotifDispatch } from "../contexts/NotifContext";
 
 const AnecdoteForm = () => {
+  const addNotification = useNotifDispatch();
   const queryClient = useQueryClient();
   const anecdoteMutation = useMutation(createAnecdote, {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData("anecdotes");
       queryClient.setQueryData("anecdotes", anecdotes.concat(newAnecdote));
     },
+    onError: () => {
+      addNotification("too short anecdote, must have length 5 or more");
+    },
   });
 
-  const onCreate = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
     const newAnecdote = {
       content: event.target.anecdote.value,
@@ -25,7 +30,7 @@ const AnecdoteForm = () => {
   return (
     <div>
       <h3>create new</h3>
-      <form onSubmit={onCreate}>
+      <form onSubmit={submitHandler}>
         <input name="anecdote" />
         <button type="submit">create</button>
       </form>
